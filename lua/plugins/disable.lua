e0639c0e673fc0f559b300fdf2f90ec830d3c91d
shey-kail <shey-kail@outlook.com>
2025-04-07 21:48:34 +0800

    change disable list

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -13,30 +13,6 @@ return {
     enabled = false,
   },
 
-  -- disable neo-tree (file explorer)
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    enabled = false,
-  },
-
-  -- disable nvim-spectre (find/replace in multiple files)
-  {
-    "nvim-pack/nvim-spectre",
-    enabled = false,
-  },
-
-  -- disable alpha-nvim (dashboard)
-  {
-    "goolord/alpha-nvim",
-    enabled = false,
-  },
-
-  -- disable nvim-notify (better notify)
-  {
-    "rcarriga/nvim-notify",
-    enabled = false,
-  },
-
   -- disable mini.ai (extend and create `a`/`i` textobjects)
   {
     "echasnovski/mini.ai",
